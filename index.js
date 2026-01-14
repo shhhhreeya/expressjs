f6ed567758160db491836fee717389fd0f9cdccb
Shreeya <shreeyajaiswal2006@gmail.com>
2026-01-14 12:33:20 +0530

    implement logging with Winston and Morgan; add logger configuration and update dependencies

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import 'dotenv/config'
 import express from 'express'
-const app = express()
+import logger from "./logger.js";
+import morgan from "morgan";
 
+const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = []
 let nextID = 1
 
 //add a new tea
 app.post('/teas', (req, res) => {
+    logger.info("A post request was made to a new tea")
     const {name, price} = req.body
     const newTea = {id: nextID++, name, price}
     teaData.push(newTea)
